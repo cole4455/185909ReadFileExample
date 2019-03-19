59a1565e9a0541996d81222d6d116b759e6588a2
cole4455 <47399988+cole4455@users.noreply.github.com>
2019-03-19 10:03:12 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _185909FileReadExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr =
+             new System.IO.StreamReader("birthdayList.txt");
+                LblOutput.Content = "";
+                while (!sr.EndOfStream)
+                {
+                    LblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                }
+                sr.Close();
+            }//end try
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }//end catch
+        }
+
+        private void Click2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr =
+             new System.IO.StreamReader("Birthday.txt");
+                LblOutput.Content = "";
+                string output = "";
+                while (!sr.EndOfStream)
+                {
+                    string fullname = sr.ReadLine();
+                    string[] names =
+                        fullname.Split(new char[] { ',' });
+                    output += names[2] + Environment.NewLine;
+                }
+
+                LblOutput.Content = output;
+                sr.Close();
+            }//end try
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }//end catch
+
+        }
+    }
+}
+
